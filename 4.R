7ed96618d06e94ffc021651845fb186e74279cf6
ravinpradhitya <90445721+ravinpradhitya@users.noreply.github.com>
2022-04-10 22:50:21 +0700

    Add files via upload

---
+++ 4.R
@@ -0,0 +1,21 @@
+#No 4
+#a
+x = 2
+v = 10
+  dchisq(x, v)
+
+#b
+x = 2
+v = 10
+z = 100
+  hist(rchisq(z,v), xlab = "Data", ylab = "Frequency",main = "Grafik Histogram")
+
+#c
+x = 2
+v = 10
+miu = v
+  paste("Nilai rata-rata tersebut adalah", miu)
+
+varian = 2 * v
+  paste("Varian tersebut adalah ", varian)
+  
